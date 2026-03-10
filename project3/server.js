4be96403f5e97969719f2fe9b19cee64ef023cd4
XXHYZ <1727181899a@gmail.com>
2026-03-10 03:27:13 -0400

    Project 03 - fixed 404 not found

--- project3/server.js
+++ project3/server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const multer = require('multer')
-const fs = require('fs')
-const path = require('path')
 const geoip = require('geoip-lite');
 
 const app = express()
@@ -14,23 +12,12 @@ app.use(express.urlencoded({ extended: true}))
 app.set('view engine', 'ejs')
 app.set('trust proxy', true)
 
-const POSTS_FILE = path.join(__dirname, 'posts.json')
-
-function loadPosts() {
-    if (!fs.existsSync(POSTS_FILE)) return [];
-    try {
-        return JSON.parse(fs.readFileSync(POSTS_FILE, 'utf8'));
-    } catch {
-        return [];
-    }
-}
-
-function savePosts(posts) {
-    fs.writeFileSync(POSTS_FILE, JSON.stringify(posts, null, 2), 'utf8');
-}
+const posts = [
+    
+];
 
 app.get('/project3/', (request, response) => {
-    response.render('index.ejs', { clientPosts: loadPosts() });
+    response.render('index.ejs', { clientPosts: posts });
 })
 
 app.post('/submit', (request, response) => {
@@ -44,7 +31,6 @@ app.post('/submit', (request, response) => {
         const location = geo
             ? [geo.city, geo.country].filter(Boolean).join(', ')
             : isLocal ? 'local' : null;
-        const posts = loadPosts();
         posts.push({
             name: name.trim(),
             email: email.trim(),
@@ -53,9 +39,10 @@ app.post('/submit', (request, response) => {
             ip,
             location
         });
-        savePosts(posts);
     }
-    response.redirect('/#join');
+    response.redirect('/project3/#join');
 })
 
-app.listen(11451, () => {})
+app.listen(11451, () => {
+    console.log('hello')
+})
